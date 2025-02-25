f15ee6a052563619146799c3c3980cdea01c2b98
Kynetiq <kinetikeith@protonmail.com>
2025-02-25 16:10:23 -0500

    Added all book models and download button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { useDropzone } from "react-dropzone";
-import BookDisplay from "./components/BookDisplay";
-import { useRef, useState } from "react";
+import BookDisplay, { BookType } from "./components/BookDisplay";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 import { HexColorPicker } from "react-colorful";
+import {
+  Button,
+  Input,
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import FileSaver from "file-saver";
+import { parse as pathParse } from "path-browserify";
 
 /* https://stackoverflow.com/questions/18650168/convert-blob-to-base64 */
 function blobToDataUrl(blob: Blob): Promise<string> {
@@ -12,12 +22,26 @@ function blobToDataUrl(blob: Blob): Promise<string> {
   });
 }
 
+const bookTypeLabels = new Map<BookType, string>([
+  [BookType.PerfectBound, "Perfect bound"],
+  [BookType.Hardcover, "Hardcover"],
+  [BookType.Saddlestitch, "Saddlestitch"],
+]);
+
 export default function App() {
   const coverInputRef = useRef<HTMLInputElement>(null);
   const spineInputRef = useRef<HTMLInputElement>(null);
   const [coverUrl, setCoverUrl] = useState("/template-cover.png");
   const [spineUrl, setSpineUrl] = useState("/template-spine.png");
   const [backColor, setBackColor] = useState("#3db999");
+  const [backColorTemp, setBackColorTemp] = useState("#3db999");
+  const [bookType, setBookType] = useState<BookType>(BookType.PerfectBound);
+
+  const [fileName, setFileName] = useState("Untitled");
+
+  useEffect(() => {
+    setBackColorTemp(backColor);
+  }, [backColor]);
 
   const {
     getRootProps: getCoverRootProps,
@@ -28,11 +52,12 @@ export default function App() {
       const file = acceptedFiles[0];
 
       if (file?.type === "image/png") {
-        console.log("is png");
         setCoverUrl(await blobToDataUrl(file));
       } else if (file?.type === "image/vnd.adobe.photoshop") {
-        console.log("is psd");
+        console.log("PSD");
       }
+
+      setFileName(pathParse(file.name).name);
     },
     accept: {
       //'image/vnd.adobe.photoshop': ['.psd'],
@@ -48,10 +73,9 @@ export default function App() {
         const file = acceptedFiles[0];
 
         if (file?.type === "image/png") {
-          console.log("is png");
           setSpineUrl(await blobToDataUrl(file));
         } else if (file?.type === "image/vnd.adobe.photoshop") {
-          console.log("is psd");
+          console.log("PSD");
         }
       },
       accept: {
@@ -61,14 +85,51 @@ export default function App() {
       noClick: true,
       noKeyboard: true,
     });
+
+  const triggerDownload = useCallback(() => {
+    const canvas: HTMLCanvasElement | null = document.querySelector("canvas");
+    console.log(document.querySelectorAll("canvas"));
+    if (canvas !== null)
+      canvas.toBlob(
+        (blob) => FileSaver.saveAs(blob, `${fileName}.png`),
+        "image/png",
+      );
+  }, []);
+
   return (
     <>
-      <section className="fixed right-4 top-4 flex flex-col items-center space-y-4 bg-gray-900 p-4 rounded-xl">
+      <section className="fixed right-4 top-4 flex flex-col items-stetch space-y-4 bg-gray-900 p-4 rounded-xl">
+        <div>
+          <label
+            htmlFor="book_type"
+            className="block mb-2 text-sm font-medium text-white"
+          >
+            Book Type
+          </label>
+          <div className="relative">
+            <Listbox value={bookType} onChange={setBookType}>
+              <ListboxButton className="border text-sm rounded-lg block w-full text-left p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500">
+                {bookTypeLabels.get(bookType)}
+              </ListboxButton>
+              <ListboxOptions className="absolute inset-x-0 top-0 border text-sm rounded-lg block overflow-clip w-full bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500">
+                {[...bookTypeLabels.entries()].map(([bookType, label]) => (
+                  <ListboxOption
+                    value={bookType}
+                    key={bookType}
+                    className="p-2.5 hover:bg-gray-800 cursor-pointer"
+                  >
+                    {label}
+                  </ListboxOption>
+                ))}
+              </ListboxOptions>
+            </Listbox>
+          </div>
+        </div>
         <div className="flex items-center justify-center h-full">
           <label
             {...getCoverRootProps({
               className:
-                "flex flex-col items-center justify-center transition-colors border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500",
+                "flex flex-col items-center justify-center transition-colors border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-800 bg-gray-900 hover:bg-gray-100 border-gray-600 hover:border-gray-500",
             })}
           >
             <div className="flex flex-col items-center justify-center py-4 px-6">
@@ -76,7 +137,7 @@ export default function App() {
               {isDragActive ? (
                 <p className="mb-2 text-sm text-gray-500">Drop Cover Here</p>
               ) : (
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <p className="mb-2 text-sm text-gray-400">
                   <span className="font-semibold">Click to upload</span> or drag
                   and drop
                 </p>
@@ -85,36 +146,66 @@ export default function App() {
             <input {...getCoverInputProps()} ref={coverInputRef} />
           </label>
         </div>
-        <div className="flex items-center justify-center h-full">
-          <label
-            {...getSpineRootProps({
-              className:
-                "flex flex-col items-center justify-center transition-colors border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500",
-            })}
-          >
-            <div className="flex flex-col items-center justify-center py-4 px-6">
-              <div className="text-lg font-bold text-gray-500">Spine</div>
-              {isDragActive ? (
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  Drop spine Here
-                </p>
-              ) : (
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click to upload</span> or drag
-                  and drop
-                </p>
-              )}
-            </div>
-            <input {...getSpineInputProps()} ref={spineInputRef} />
-          </label>
-        </div>
-        <HexColorPicker color={backColor} onChange={setBackColor} />
+        {[BookType.PerfectBound, BookType.Hardcover].includes(bookType) ? (
+          <div className="flex items-center justify-center h-full">
+            <label
+              {...getSpineRootProps({
+                className:
+                  "flex flex-col items-center justify-center transition-colors border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-800 bg-gray-900 hover:bg-gray-100 border-gray-600 hover:border-gray-500",
+              })}
+            >
+              <div className="flex flex-col items-center justify-center py-4 px-6">
+                <div className="text-lg font-bold text-gray-500">Spine</div>
+                {isDragActive ? (
+                  <p className="mb-2 text-sm text-gray-400">Drop spine Here</p>
+                ) : (
+                  <p className="mb-2 text-sm text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                )}
+              </div>
+              <input {...getSpineInputProps()} ref={spineInputRef} />
+            </label>
+          </div>
+        ) : null}
+        {bookType === BookType.Hardcover ? (
+          <div className="space-y-4">
+            <label
+              htmlFor="color_hex"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Back Cover
+            </label>
+            <HexColorPicker color={backColor} onChange={setBackColor} />
+            <Input
+              type="text"
+              id="color_hex"
+              className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+              placeholder="#ffffff"
+              value={backColorTemp}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                const value = event.target.value;
+                if (value.length > 7) return;
+                if (/^#([0-9A-F]{3}){1,2}$/i.test(value)) setBackColor(value);
+                setBackColorTemp(value);
+              }}
+            />
+          </div>
+        ) : null}
+        <Button
+          onClick={triggerDownload}
+          className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800 w-full"
+        >
+          Download Image
+        </Button>
       </section>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <BookDisplay
           coverUrl={coverUrl}
           spineUrl={spineUrl}
           backColor={backColor}
+          bookType={bookType}
         />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
--- src/components/BookDisplay.tsx
+++ src/components/BookDisplay.tsx
@@ -4,31 +4,152 @@ import {
   AccumulativeShadows,
   Environment,
   RandomizedLight,
+  useGLTF,
 } from "@react-three/drei";
 import { EffectComposer, N8AO, SMAA } from "@react-three/postprocessing";
+import { Texture } from "three";
+
+export enum BookType {
+  PerfectBound,
+  Hardcover,
+  Saddlestitch,
+}
+
+function HardcoverBook({
+  coverMap,
+  spineMap,
+  backColor,
+}: {
+  coverMap: Texture;
+  spineMap: Texture;
+  backColor: string;
+}) {
+  const pageMap = useLoader(TextureLoader, "page-effect.png");
+  const coverAspect = coverMap.image.width / coverMap.image.height;
+  const spineAspect = spineMap.image.width / spineMap.image.height;
+  return (
+    <group>
+      <mesh castShadow receiveShadow position={[0, 0, spineAspect / 2]}>
+        <planeGeometry args={[coverAspect, 1]} />
+        <meshStandardMaterial map={coverMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        rotation={[0, -Math.PI / 2.0, 0]}
+        position={[-coverAspect / 2, 0, 0]}
+      >
+        <planeGeometry args={[spineAspect, 1]} />
+        <meshStandardMaterial map={spineMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        rotation={[-Math.PI / 2.0, 0, 0]}
+        position={[0, 0.49, 0]}
+      >
+        <planeGeometry args={[coverAspect, spineAspect]} />
+        <meshStandardMaterial map={pageMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh castShadow receiveShadow position={[0, 0, -spineAspect / 2]}>
+        <planeGeometry args={[coverAspect, 1]} />
+        <meshStandardMaterial side={2} shadowSide={2} color={backColor} />
+      </mesh>
+      <mesh castShadow position={[0, -0.25, 0]}>
+        <boxGeometry args={[coverAspect - 0.01, 0.5, spineAspect - 0.01]} />
+        <shadowMaterial />
+      </mesh>
+    </group>
+  );
+}
+
+function PerfectBoundBook({
+  coverMap,
+  spineMap,
+}: {
+  coverMap: Texture;
+  spineMap: Texture;
+}) {
+  const pageMap = useLoader(TextureLoader, "page-effect.png");
+  const coverAspect = coverMap.image.width / coverMap.image.height;
+  const spineAspect = spineMap.image.width / spineMap.image.height;
+  return (
+    <group>
+      <mesh castShadow receiveShadow position={[0, 0, spineAspect / 2]}>
+        <planeGeometry args={[coverAspect, 1]} />
+        <meshStandardMaterial map={coverMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        rotation={[0, -Math.PI / 2.0, 0]}
+        position={[-coverAspect / 2, 0, 0]}
+      >
+        <planeGeometry args={[spineAspect, 1]} />
+        <meshStandardMaterial map={spineMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        rotation={[-Math.PI / 2.0, 0, 0]}
+        position={[0, 0.5, 0]}
+      >
+        <planeGeometry args={[coverAspect, spineAspect]} />
+        <meshStandardMaterial map={pageMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh castShadow position={[0, -0.25, 0]}>
+        <boxGeometry args={[coverAspect - 0.01, 0.5, spineAspect - 0.01]} />
+        <shadowMaterial />
+      </mesh>
+    </group>
+  );
+}
+
+function SaddlestitchBook({ coverMap }: { coverMap: Texture }) {
+  const pageMap = useLoader(TextureLoader, "/page-effect.png");
+  const coverAspect = coverMap.image.width / coverMap.image.height;
+  const { nodes } = useGLTF("/saddlestitch.glb");
+  return (
+    <group scale={[coverAspect, 1, 1]}>
+      <mesh receiveShadow geometry={nodes.Cover.geometry}>
+        <meshStandardMaterial map={coverMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh receiveShadow geometry={nodes.Pages.geometry}>
+        <meshStandardMaterial map={pageMap} side={2} shadowSide={2} />
+      </mesh>
+      <mesh
+        castShadow
+        geometry={nodes.Cover.geometry}
+        scale={[0.98, 0.98, 0.98]}
+        position={[0, -0.01, 0]}
+      >
+        <meshStandardMaterial shadowSide={2} />
+      </mesh>
+    </group>
+  );
+}
 
 export default function BookDisplay({
   coverUrl,
   spineUrl,
   backColor,
+  bookType,
 }: {
   coverUrl: string;
   spineUrl: string;
   backColor: string;
+  bookType: BookType;
 }) {
   const coverMap = useLoader(TextureLoader, coverUrl);
   const spineMap = useLoader(TextureLoader, spineUrl);
-  const pageMap = useLoader(TextureLoader, "page-effect.png");
-  const coverAspect = coverMap.image.width / coverMap.image.height;
-  const spineAspect = spineMap.image.width / spineMap.image.height;
 
   return (
     <div className="w-[500px] h-[600px]">
       <Canvas
         shadows
         orthographic
-        camera={{ position: [-5, 2, 10], zoom: 400, near: 1, far: 20 }}
-        gl={{ antialias: true }}
+        camera={{ position: [-4, 1.5, 10], zoom: 400, near: 1, far: 20 }}
+        gl={{ antialias: true, preserveDrawingBuffer: true }}
         dpr={[2, 4]}
       >
         <Environment files={["public/environment.hdr"]} />
@@ -42,45 +163,26 @@ export default function BookDisplay({
           />
           <SMAA />
         </EffectComposer>
-        <group>
-          <mesh castShadow receiveShadow>
-            <planeGeometry args={[coverAspect, 1]} />
-            <meshStandardMaterial map={coverMap} side={2} shadowSide={2} />
-          </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            rotation={[0, -Math.PI / 2.0, 0]}
-            position={[-coverAspect / 2, 0, -spineAspect / 2]}
-          >
-            <planeGeometry args={[spineAspect, 1]} />
-            <meshStandardMaterial map={spineMap} side={2} shadowSide={2} />
-          </mesh>
-          <mesh
-            castShadow
-            receiveShadow
-            rotation={[-Math.PI / 2.0, 0, 0]}
-            position={[0, 0.49, -spineAspect / 2]}
-          >
-            <planeGeometry args={[coverAspect, spineAspect]} />
-            <meshStandardMaterial map={pageMap} side={2} shadowSide={2} />
-          </mesh>
-          <mesh castShadow receiveShadow position={[0, 0, -spineAspect]}>
-            <planeGeometry args={[coverAspect, 1]} />
-            <meshStandardMaterial side={2} shadowSide={2} color={backColor} />
-          </mesh>
-        </group>
+        {bookType === BookType.Hardcover ? (
+          <HardcoverBook
+            coverMap={coverMap}
+            spineMap={spineMap}
+            backColor={backColor}
+          />
+        ) : null}
+        {bookType === BookType.PerfectBound ? (
+          <PerfectBoundBook coverMap={coverMap} spineMap={spineMap} />
+        ) : null}
+        {bookType === BookType.Saddlestitch ? (
+          <SaddlestitchBook coverMap={coverMap} />
+        ) : null}
         <mesh
           castShadow
           rotation={[-Math.PI / 2.0, 0, 0]}
           position={[0, -0.501, 0]}
         >
-          <planeGeometry args={[coverAspect, spineAspect]} />
+          <planeGeometry args={[2, 2]} />
           <shadowMaterial transparent opacity={0} />
-        </mesh>
-        <mesh castShadow position={[0, -0.25, -spineAspect / 2]}>
-          <boxGeometry args={[coverAspect - 0.01, 0.5, spineAspect - 0.01]} />
-          <shadowMaterial />
         </mesh>
         <AccumulativeShadows
           position={[0, -0.5, 0]}
