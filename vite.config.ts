197a22d9915ce5e8c633ab14588031d6acf49467
Kynetiq <kinetikeith@protonmail.com>
2025-02-25 22:45:11 -0500

    Fixed url issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/bookmaker/",
+  base: "/book-maker/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
