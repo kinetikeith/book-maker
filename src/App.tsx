9183f651caa6e7add9acf664052bd646904f09b4
Kynetiq <kinetikeith@protonmail.com>
2025-02-27 16:15:14 -0500

    Fixed copy button click shifting

--- src/App.tsx
+++ src/App.tsx
@@ -290,7 +290,7 @@ export default function App() {
         <Button
           onClick={triggerCopy}
           data-copied={copied ? true : undefined}
-          className="text-blue-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 border bg-green-600/0 border-1 border-blue-500 hover:border-blue-300 hover:text-blue-300 focus:outline-none focus:ring-blue-800 w-full data-[copied=true]:border-none data-[copied=true]:text-white data-[copied=true]:bg-green-600 transition-colors duration-500"
+          className="text-blue-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 border bg-green-600/0 border-1 border-blue-500 hover:border-blue-300 hover:text-blue-300 focus:outline-none focus:ring-blue-800 w-full data-[copied=true]:border-transparent data-[copied=true]:text-white data-[copied=true]:bg-green-600 transition-colors duration-500"
         >
           Copy Image
         </Button>
